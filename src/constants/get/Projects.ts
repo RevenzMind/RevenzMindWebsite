39fb90992ac93da0261b62550907e4a518222a26
Your Name <you@example.com>
2024-01-13 04:47:10 -0600

    🔥 Add Portfolio

---
+++ src/constants/get/Projects.ts
@@ -0,0 +1,29 @@
+export const GetProjects = [
+    {
+        emoji: "🖱️",
+        name: "Roblox MouseFix ",
+        description: "Fix cursor in roblox uwp",
+        hasdeployed: false,
+        deployedLink: "",
+        codeLink: "https://github.com/RevenzMind/RBXMouseFix",
+    },
+    {
+        emoji: "🎮",
+        name: "Roblox Autoclicker",
+        description: "Autoclicker for roblox with customizable cps and hotkeys",
+        hasdeployed: false,
+        deployedLink: "",
+        codeLink: "https://github.com/RevenzMind/Roblox-Autoclicker",
+    },
+
+];
+
+
+export interface ProjectType{
+    emoji: string;
+    name: string;
+    description: string;
+    hasdeployed: boolean;
+    deployedLink: string;
+    codeLink: string;
+}
---
+++ src/constants/get/Skills.ts
@@ -0,0 +1,23 @@
+export const Frameworks = [
+    {name: "React", imageurl: "/Assets/Skills/Framework/tsx.png"},
+    {name: "Astro", imageurl: "/Assets/Skills/Framework/astro.png"},
+    {name: "Nextjs", imageurl: "/Assets/Skills/Framework/next.png"},
+    {name: "None", imageurl: "/Assets/Skills/unknow.png"},
+];
+export const Frontend = [
+    {name: "Javascript", imageurl: "/Assets/Skills/Frontend/js.png"},
+    {name: "Typescript", imageurl: "/Assets/Skills/Frontend/ts.png"},
+
+    {name: "HTML", imageurl: "/Assets/Skills/Frontend/html.png"},
+    {name: "CSS", imageurl: "/Assets/Skills/Frontend//css.png"},
+];
+
+export const DevTools = [
+    {name: "Git", imageurl: "/Assets/Skills/DevTools/git.png"},
+    {name: "Vscode", imageurl: "/Assets/Skills/DevTools/vscode.png"},
+    {
+        name: "Visual Studio",
+        imageurl: "/Assets/Skills/DevTools/csproj.png",
+    },
+    {name: "Npm", imageurl: "/Assets/Skills/DevTools/npm.png"},
+];
